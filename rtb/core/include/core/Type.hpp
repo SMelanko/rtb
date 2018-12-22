67dff68a6161d8838b32ac8866acb9706b86988f
smela <slava.melanko@gmail.com>
2018-12-22 21:27:58 +0200

    OpenRTB: BidRequest struct

--- rtb/core/include/core/Type.hpp
+++ rtb/core/include/core/Type.hpp
@@ -5,6 +5,7 @@
 namespace Core
 {
 
+using Bool = bool;
 using Double = double;
 using Int = int;
 
--- rtb/proto/include/proto/OpenRtb.hpp
+++ rtb/proto/include/proto/OpenRtb.hpp
@@ -75,6 +75,7 @@ public:
 
 struct Deal
 {
+public:
     /// A unique identifier for the direct deal.
     Core::String id;
     /// Minimum bid for this impression expressed in CPM.
@@ -106,6 +107,7 @@ struct Deal
 
 struct Pmp
 {
+public:
     /// Indicator of auction eligibility to seats named in the Direct Deals object,
     /// where 0 = all bids are accepted, 1 = bids are restricted to the deals specified and the terms thereof.
     Core::Int private_auction;
@@ -129,6 +131,7 @@ struct Pmp
 
 struct Impression
 {
+public:
     /// A unique identifier for this impression within the context of the bid request.
     Core::String id;
     /// A Banner object (Section 3.2.3); required if this impression is offered as a banner ad opportunity.
@@ -162,6 +165,65 @@ struct Impression
     Core::Vector<Core::String> iframebuster;
     /// A Pmp object (Section 3.2.17) containing any private marketplace deals in effect for this impression.
     Core::Opt<Pmp> pmp;
+    /// Placeholder for exchange-specific extensions to OpenRTB.
+    Json::Document ext;
+};
+
+/*
+ * 3.2.1 Object: BidRequest
+ *
+ * The top-level bid request object contains a globally unique bid request or auction ID. This id attribute
+ * is required as is at least one impression object (Section 3.2.2). Other attributes in this top-level object
+ * establish rules and restrictions that apply to all impressions being offered.
+ * There are also several subordinate objects that provide detailed data to potential buyers. Among these are
+ * the Site and App objects, which describe the type of published media in which the impression(s) appear.
+ * These objects are highly recommended, but only one applies to a given bid request depending on
+ * whether the media is browser-based web content or a non-browser application, respectively.
+ */
+
+struct BidRequest
+{
+public:
+    /// Unique ID of the bid request, provided by the exchange.
+    Core::String id;
+    /// Array of Imp objects (Section 3.2.2) representing the impressions offered. At least 1 Imp object is required.
+    Core::Vector<Impression> imp;
+#if 0
+    /// Details via a Site object (Section 3.2.6) about the publisher’s website.
+    /// Only applicable and recommended for websites.
+    Site site;
+    /// Details via an App object (Section 3.2.7) about the publisher’s app (i.e., non-browser applications).
+    /// Only applicable and recommended for apps.
+    App app;
+    /// Details via a Device object (Section 3.2.11) about the user’s device to which the impression will be delivered.
+    Device device;
+    /// Details via a User object (Section 3.2.13) about the human user of the device; the advertising audience.
+    User user;
+#endif
+    /// Indicator of test mode in which auctions are not billable, where 0 (false) = live mode, 1 (true) = test mode.
+    Core::Bool test = false;
+    /// Auction type, where 1 = First Price, 2 = Second Price Plus.
+    /// Exchange-specific auction types can be defined using values greater than 500.
+    AuctionPrice at = AuctionPrice::SECOND_PRICE_PLUS;
+    /// Maximum time in milliseconds to submit a bid to avoid timeout. This value is commonly communicated offline.
+    Core::Int tmax;
+    /// Whitelist of buyer seats allowed to bid on this impression.
+    /// Seat IDs must be communicated between bidders and the exchange a priori. Omission implies no seat restrictions.
+    Core::Vector<Core::String> wseat;
+    /// Flag to indicate if Exchange can verify that the impressions offered represent all of the impressions available
+    /// in context (e.g., all on the web page, all video spots such as pre/mid/post roll) to support road-blocking.
+    /// 0 = no or unknown, 1 = yes, the impressions offered represent all that are available.
+    Core::Bool allimps = false;
+    /// Array of allowed currencies for bids on this bid request using ISO-4217 alpha codes.
+    /// Recommended only if the exchange accepts multiple currencies.
+    Core::Vector<Core::String> cur;
+    /// Blocked advertiser categories using the IAB content categories. Refer to List 5.1.
+    Core::Vector<Core::String> bcat;
+    /// Block list of advertisers by their domains (e.g., “ford.com”).
+    Core::Vector<Core::String> badv;
+    /// A Regs object (Section 3.2.16) that specifies any industry, legal,
+    /// or governmental regulations in force for this request.
+    Json::Document regs;
     /// Placeholder for exchange-specific extensions to OpenRTB.
     Json::Document ext;
 };
