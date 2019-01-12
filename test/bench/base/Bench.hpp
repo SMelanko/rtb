a40855226b9d48daf73f71fee8d80bd3a162ca6d
Slava Melanko <v.melanko@avantize.com>
2019-01-12 15:36:54 +0200

    1М benchmark iterations

--- test/bench/base/Bench.hpp
+++ test/bench/base/Bench.hpp
@@ -3,4 +3,4 @@
 #include <benchmark/benchmark.h>
 namespace bench = benchmark;
 
-#define BENCH(x) BENCHMARK(x)->Iterations(100'000)->Unit(bench::kNanosecond)
+#define BENCH(x) BENCHMARK(x)->Iterations(1'000'000)->Unit(bench::kNanosecond)
